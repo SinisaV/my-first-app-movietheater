48b3a9a3988ccc61df45fd46961f60453ccc0ae5
sinisaV <sinisa.vucetic@student.um.si>
2022-11-14 22:38:22 +0100

    Added two new activities and used registerForActivityResult for getting elements from MainActivity and added qrCode Scanner to fill editTexts.

---
+++ app/src/main/java/com/example/movieteather/EntranceActivity.kt
@@ -0,0 +1,53 @@
+package com.example.movieteather
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.lib.AdvanceTicket
+import com.example.movieteather.databinding.ActivityEntranceBinding
+
+class EntranceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEntranceBinding
+
+    var listOfAdvanceTickets = mutableListOf<AdvanceTicket>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEntranceBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        fun onActivityResult(result:ActivityResult) {
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+
+                val id: Int = data!!.getIntExtra("id", 1111)
+                val price: Double = data.getDoubleExtra("price", 0.0)
+                val daysAhead: Int = data.getIntExtra("daysAhead", 0)
+
+                listOfAdvanceTickets.add(
+                    AdvanceTicket(id, price, daysAhead)
+                )
+                binding.textViewResult.text = (listOfAdvanceTickets.toString())
+            }
+        }
+
+        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            onActivityResult(result)
+        }
+
+        binding.buttonMainActivity.setOnClickListener() {
+            val intent = Intent(this, MainActivity::class.java)
+            getContent.launch(intent)
+        }
+
+        binding.buttonAboutActivity.setOnClickListener() {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/movieteather/MainActivity.kt
+++ app/src/main/java/com/example/movieteather/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.example.movieteather
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.widget.Toast
 import com.example.lib.AdvanceTicket
-import com.example.lib.Theatre
-
 import com.example.movieteather.databinding.ActivityMainBinding
+import io.github.g00fy2.quickie.ScanQRCode
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }*/
 
     private lateinit var binding: ActivityMainBinding
     var listOfAdvanceTickets = mutableListOf<AdvanceTicket>()
@@ -26,12 +22,38 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val scanQrCodeLauncher = registerForActivityResult(ScanQRCode()) { result ->
+
+            val getResultString: String = result.toString().substringAfter("Value=").substringBefore(")")
+            //Toast.makeText(applicationContext, getResultString, Toast.LENGTH_SHORT).show()
+            val splitResult = " "
+            val resultList = getResultString.split(splitResult)
+            val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+
+            for (i in resultList) {
+                if (resultList.count() != 3) {
+                    Toast.makeText(applicationContext, "ERROR COUNT", Toast.LENGTH_SHORT).show()
+                }
+                else if (!resultList[0].matches(regex) || !resultList[1].matches(regex) || !resultList[2].matches(regex)) {
+                    Toast.makeText(applicationContext, "ID, price or daysAhead contains only numbers!", Toast.LENGTH_SHORT).show()
+                }
+                else if (resultList[0].contains(".") || resultList[2].contains(".")) {
+                    Toast.makeText(applicationContext, "ID and DaysAhead cant have decimal numbers!", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    binding.editTextTicketId.setText(resultList[0])
+                    binding.editTextTicketPrice.setText(resultList[1])
+                    binding.editTextTicketDaysAhead.setText(resultList[2])
+                }
+            }
+        }
+
         binding.buttonAddTicket.setOnClickListener() {
 
             if (binding.editTextTicketId.text.isBlank() || binding.editTextTicketPrice.text.isBlank()
                 || binding.editTextTicketDaysAhead.text.isBlank()) {
 
-                val myToast = Toast.makeText(applicationContext, "Enter again!!!", Toast.LENGTH_SHORT);
+                val myToast = Toast.makeText(applicationContext, "Enter again!!!", Toast.LENGTH_SHORT)
                 myToast.setGravity(Gravity.CENTER, 0, 0)
                 myToast.show()
             }
@@ -45,14 +67,27 @@ class MainActivity : AppCompatActivity() {
                 binding.editTextTicketId.text.clear()
                 binding.editTextTicketPrice.text.clear()
                 binding.editTextTicketDaysAhead.text.clear()
+
+                val data = Intent()
+
+                data.putExtra("id", id)
+                data.putExtra("price",price)
+                data.putExtra("daysAhead", daysAhead)
+
+                setResult(RESULT_OK, data)
+                finish()
             }
         }
 
-        binding.buttonInfo.setOnClickListener() {
-            Log.i("List size: ", listOfAdvanceTickets.size.toString());
+        /*binding.buttonInfo.setOnClickListener() {
+            Log.i("List size: ", listOfAdvanceTickets.size.toString())
+        }*/
+
+        binding.buttonScanQRCode.setOnClickListener {
+            scanQrCodeLauncher.launch(null)
         }
         binding.buttonExit.setOnClickListener() {
-            exitProcess(0);
+            exitProcess(0)
         }
     }
 }
